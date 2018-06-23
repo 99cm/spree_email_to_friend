376e2b8988773ed94a812c2dd5f3889909a39747
99cm <zyizyao@china-guide.com>
2018-06-23 16:34:58 -0400

    fix syntax errors

--- app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
+++ app/overrides/add_captcha_settings_to_admin_configurations_menu.rb
@@ -2,5 +2,4 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_configuration_menu',
   name: 'add_captcha_admin_option',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<%= configurations_sidebar_menu_item t('spree.captcha_settings', scope: :captcha), edit_admin_captcha_settings_path %>'
-)
+  text: '<%= configurations_sidebar_menu_item t('captcha_settings', scope: :captcha), edit_admin_captcha_settings_path %>' )
